2514cf9af080ebfe75e9a3724e67b1cd53e8a23f
Thuy <thuythdao@gmail.com>
2017-10-25 17:05:58 -0400

    Correct class_name in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :title, :body, :author_id, presence: true
 
   belongs_to :author,
-    classname: 'User',
+    class_name: 'User',
     primary_key: :id,
     foreign_key: :author_id
 
