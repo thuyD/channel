5c35886a603bfa195d26464ca99e6d24512da739
Thuy <thuythdao@gmail.com>
2017-10-26 11:53:56 -0400

    Seed the database with demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  username: "Chirps",
+  password: "catnip",
+  name: "Chirps the Zero",
+  bio: "I am a professinal cat blogger with revolutionary ideas. Everyone should follow me and claps at all of my genius creations. You're WELCOME!")
