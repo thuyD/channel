23895afe94ace2fc71a0b9e0f49a6153a04c3d9e
Thuy <thuythdao@gmail.com>
2017-11-16 11:06:27 -0500

    Fix repeating cover from post index

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,20 @@
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
 import RootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     RootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(thunk, logger))
+    composeEnhancers(applyMiddleware(...middlewares))
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
 
 module.exports = {
   context: __dirname,
